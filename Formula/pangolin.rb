32551b904ea342568d29eb16504688661e41e4ea
IvanB <fhill122@gmail.com>
2022-07-31 08:59:38 +0800

    [pangolin] update

--- Formula/pangolin.rb
+++ Formula/pangolin.rb
@@ -1,25 +1,24 @@
 class Pangolin < Formula
-    desc "Lightweight portable rapid development library for managing OpenGL display / interaction and abstracting video input"
-    homepage "https://github.com/stevenlovegrove/Pangolin"
-    url "https://github.com/stevenlovegrove/Pangolin/archive/refs/tags/v0.8.tar.gz"
-    sha256 "a680d1b52cd432f93f3359cb8f2179b25acf384d29dbaa2530b2eb3dfe5a81c6"
-    license "MIT"
-  
-    depends_on "cmake" => :build
-    depends_on "eigen"
-    depends_on "glew"
-  
-    def install
-      mkdir "build" do
-        # ENV["CMAKE_PREFIX_PATH"] = "#{Formula["qt@5"].lib}/cmake"
-        system "cmake", "..", *std_cmake_args
-        system "make"
-        system "make", "install"
-      end
-    end
-  
-    test do
-      system "true"
+  desc "Library for managing OpenGL display / interaction and abstracting video input"
+  homepage "https://github.com/stevenlovegrove/Pangolin"
+  url "https://github.com/stevenlovegrove/Pangolin/archive/refs/tags/v0.8.tar.gz"
+  sha256 "a680d1b52cd432f93f3359cb8f2179b25acf384d29dbaa2530b2eb3dfe5a81c6"
+  license "MIT"
+
+  depends_on "cmake" => :build
+  depends_on "eigen"
+  depends_on "glew"
+
+  def install
+    mkdir "build" do
+    # ENV["CMAKE_PREFIX_PATH"] = "#{Formula["qt@5"].lib}/cmake"
+    system "cmake", "..", *std_cmake_args, "-DBUILD_PANGOLIN_FFMPEG=OFF"
+    system "make"
+    system "make", "install"
     end
   end
-  
+
+  test do
+    system "true"
+  end
+end
