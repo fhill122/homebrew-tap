6b069aab8c163a328aae577f8abee33cc7e5e31a
IvanB <fhill122@gmail.com>
2022-08-30 14:52:45 +0800

    [pangolin] switch version to v0.6

--- Formula/pangolin.rb
+++ Formula/pangolin.rb
@@ -1,8 +1,9 @@
 class Pangolin < Formula
   desc "Library for managing OpenGL display / interaction and abstracting video input"
   homepage "https://github.com/stevenlovegrove/Pangolin"
-  url "https://github.com/stevenlovegrove/Pangolin/archive/refs/tags/v0.8.tar.gz"
-  sha256 "a680d1b52cd432f93f3359cb8f2179b25acf384d29dbaa2530b2eb3dfe5a81c6"
+  url "https://github.com/stevenlovegrove/Pangolin/archive/refs/tags/v0.6.tar.gz"
+  # sha256 "a680d1b52cd432f93f3359cb8f2179b25acf384d29dbaa2530b2eb3dfe5a81c6" # v0.8
+  sha256 "2fe0f7242bee9b7241a73d17878fe25179f22b07a18d02dec069e4c90890e9d5" # v0.6
   license "MIT"
 
   depends_on "cmake" => :build
